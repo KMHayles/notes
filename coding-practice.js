bf3abfa273b21778c5ca18445c6795f13e7e424a
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2022-12-01 09:57:29 -0600

    add notes for loops

---
+++ coding-practice.js
@@ -0,0 +1,55 @@
+// Loops notes and examples
+//
+// while (condition){
+//     (body
+
+// do {
+//     (body of code)
+// console.log();
+//the iteration will go below the body of code
+// } while(condition); if you have to do something 'once' a do while will always run.
+
+// for (intializationl; condition; increment) {
+//
+// }
+
+// for(let i = 10; i > 0; i-=1) {
+//     console.log(`For Loop #${i}`);
+// }
+//
+// loop through an array, and print the string in each item in a sentence;
+
+// let array = ['oranges', 'apples', 'pineapples','pears', 'grapes', 'tomatoes'];
+// for(let i = 0; i < array.length; i++){
+// //     console.log(`${array[i]} are awesome!`);
+// }
+//
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'tomatoes'){
+//         console.log(`Wait..${array[1]} are a fruit?!`);
+//     } else {
+//         fruitMessage(array[i]);
+//     }
+// }
+//
+// function fruitMessage(input){
+//     console.log(`${input} are pretty cool!`);
+// }
+
+// let array = ['oranges', 'apples', 'pineapples','pears', 'grapes', 'tomatoes'];
+// for(let i = 0; i < array.length; i++){
+// // //     console.log(`${array[i]} are awesome!`);
+// }
+//
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'tomatoes'){
+//         console.log(`I found you mr.tomatoes`);
+//         break;
+//     } else {
+//         fruitMessage(array[i]);
+//     }
+// }
+//
+// function fruitMessage(input){
+//     console.log(`${input} are pretty cool!`);
+// }
