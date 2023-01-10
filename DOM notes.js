f81510a7fae2115d2b0784a3fd9f73c31f3592c6
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-01-10 14:05:52 -0600

    add dom notes

---
+++ DOM notes.js
@@ -0,0 +1,36 @@
+(() => {
+    // let btn = document.querySelector('#btn1');
+    // let btn2 = document.querySelector('#btn2');
+    // //add event listener to btn1
+    //
+    // btn.addEventListener('click', () => {
+    //    alert('Great job!');
+    // });
+    //
+    // //add event listener to btn2
+    // btn2.addEventListener('click', () => {
+    //     //remove all buttons on the page.
+    //     btn.remove();
+    //     btn2.remove();
+    //     let video = document.createElement("video");
+    //     video.src = "images/bomb.mp4";
+    //     video.autoplay = true;
+    //     document.querySelector('.page-wrapper').appendChild(video);
+    // });
+    //
+
+    let header = document.querySelector('#header');
+    window.addEventListener('scroll', () => {
+       if(window.pageYOffset > 0){
+           header.classList.add('sticky');
+       } else {
+           header.className.remo
+       }
+
+    });
+    header.remove();
+
+
+
+
+})();
