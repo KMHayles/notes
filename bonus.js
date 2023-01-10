b223a8d7705ead0702b8b65f29b6fc700afd83b5
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-01-10 09:32:11 -0600

    add lecture notes

--- bonus.js
+++ bonus.js
@@ -273,7 +273,7 @@
 // console.log(filterList);
 
 
-//filterList([1,2,‘a’,‘b’]) // returns [1,2]
+// filterList([1,2,‘a’,‘b’]) // returns [1,2]
 // filterList([1,‘a’,‘b’,0,15]); // returns [1,0,15]
 // filterList([1,2,‘aasf’,‘1’,‘123’,123]); // returns [1,2,123]
 
@@ -284,3 +284,86 @@
 //             if(typeof input[i])
 //         }
 // }
+
+// 01/09/23
+
+// Create a function, zipArrays, that takes two array inputs of the same length and returns an array with the elements of both arrays alternating in the order of first[0], second[0], first[1], second[1], etc. If both arrays are empty, return an empty array.
+
+// function zipArrays(input1, input2) {
+//     let arrayOutput = [];
+//         if (input1.length === 0){
+//             return [];
+//         }
+//         for(let i=0, i<input1.length;i+=1){
+//             arrayOutput.push(input1[i]);
+//             arrayOutput.push(input2[i])[]
+//         }
+//         return arrayOutput;
+// }
+//
+//
+// console.log(zipArrays([], [])) // returns []
+// console.log(zipArrays([1], [2])) // returns [1, 2]
+// console.log(zipArrays(['a', 'b'], ['c', 'd']) // returns... ['a'), 'c', 'b', 'd']
+// console.log(zipArrays([1,2,'a','b'], ['bob', null, 'sally', 25])) // returns...
+
+//01/10/23
+
+// Write a function, getUserCredentials, that takes in a list of user objects and returns a list of user objects with only the username and password properties. Assume at least an array of one user object.
+
+
+    const users = [
+    {
+        firstName: 'Justin',
+        lastName: 'Reich',
+        dob: '1923-01-01',
+        username: 'jreich',
+        password: '$2y$10$UJlsa5vWq5DUKJjyO38gM.dCZfudWOFCrLWQosh0mhXKaZmRmvDse'
+    },
+    {
+        firstName: 'Sally',
+        lastName: 'Smith',
+        dob: '1935-03-11',
+        username: 'ssmith',
+        password: '$2y$10$VaLGU5.7uQLr.eg6kSI9seOcP4JY4XktWt28I9JgblAGIDpkDXbya'
+    },
+    {
+        firstName: 'Fred',
+        lastName: 'Smith',
+        dob: '1999-01-21',
+        username: 'fsmith',
+        password: '$2y$10$3USt6Dl8TNMkeh0KioPnfeVpynAotXvSIJ5xrzAHragPEAWMYEBNS'
+    },
+]
+
+
+function getUserCredentials(users) {
+    const credentials = [];
+    for(let i=0; i < users.length; i++){
+        const user = users[i]
+        credentials.push({ username: user.username, password: user.password});
+    }
+    return credentials;
+}
+console.log (getUserCredentials(users));
+
+
+
+/*
+
+[
+  {
+    username: 'jreich',
+    password: '$2y$10$UJlsa5vWq5DUKJjyO38gM.dCZfudWOFCrLWQosh0mhXKaZmRmvDse'
+  },
+  {
+    username: 'ssmith',
+    password: '$2y$10$VaLGU5.7uQLr.eg6kSI9seOcP4JY4XktWt28I9JgblAGIDpkDXbya'
+  },
+  {
+    username: 'fsmith',
+    password: '$2y$10$3USt6Dl8TNMkeh0KioPnfeVpynAotXvSIJ5xrzAHragPEAWMYEBNS'
+  }
+]
+
+*/
